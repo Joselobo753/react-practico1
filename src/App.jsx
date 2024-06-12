7a7184f41ab25b739a1e3324081765fc05ebb974
Jose Lobo <j.a.lobo753@gmail.com>
2024-06-12 15:45:54 -0300

    agregando PropTypes y eje2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Eje1 from "./practico/Eje1.jsx"
 
 const App = () => {
+ 
   return (
     <>
-    <h1>Hola mundo!!!!!!</h1>
+    <h1>Hola </h1>
   
-    <Eje1 />
+    <Eje1 eje2="mi amigo esto es una propType" />
     </>
   )
 }
--- src/practico/Eje1.jsx
+++ src/practico/Eje1.jsx
@@ -1,8 +1,11 @@
-
-const Eje1 = () => {
+import PropTypes from "prop-types"
+const Eje1 = (props) => {
   return (
-    <h1>Hola mundo desde un componente</h1>
+    <h1>Hola mundo, {props.eje2} </h1>
   )
+}
+Eje1.propTypes = {
+  eje2: PropTypes.string.isRequired
 }
 
 export default Eje1
