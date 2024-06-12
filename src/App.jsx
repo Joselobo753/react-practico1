ed2676d69b2b6159a5d3560f45a5f3c06b73f60e
Jose Lobo <j.a.lobo753@gmail.com>
2024-06-12 15:05:08 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Ejeone from "./practico/Eje1.jsx"
+
+const App = () => {
+  return (
+    <>
+    <h1>Hola mundo!!!!!!</h1>
+  
+    <Ejeone />
+    </>
+  )
+}
+
+export default App
---
+++ src/practico/Eje1.jsx
@@ -0,0 +1,8 @@
+
+const Ejeone = () => {
+  return (
+    <h1>Hola mundo desde un componente</h1>
+  )
+}
+
+export default Ejeone
