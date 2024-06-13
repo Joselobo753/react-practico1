cb15e707c3aa6a93dfd3efa4ffc2ebaf15cc2ca8
Jose Lobo <j.a.lobo753@gmail.com>
2024-06-13 13:32:04 -0300

    se agrega el ejercicio 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import Borrar1 from "./borrar/borrar1.jsx"
 import Eje1 from "./practico/Eje1.jsx"
+import Eje4 from "./practico/Eje4.jsx"
 
 const App = () => {
  
   return (
     <>
-    <h1>Hola </h1>
-  
+   
+    <Borrar1 />
     <Eje1 eje2="mi amigo esto es una propType" />
+    <Eje4/>
     </>
   )
 }
--- src/practico/Eje1.jsx
+++ src/practico/Eje1.jsx
@@ -1,7 +1,16 @@
 import PropTypes from "prop-types"
+import { useState } from "react"
 const Eje1 = (props) => {
+  const [tres,setTres] = useState("")
+  const eje3 = () =>{
+    setTres("Esto no es un useState es magia")
+  }
   return (
+    <section>
     <h1>Hola mundo, {props.eje2} </h1>
+    <p>{tres}</p>
+    <button onClick={eje3}> click para magia</button>
+    </section>
   )
 }
 Eje1.propTypes = {
